9af9919f613ab979fca111deece6cd855fbc4bea
Sam Hoffman <samdhoffman@gmail.com>
2020-01-30 16:21:33 -0500

    Increment start index when looking for best match

--- matching_helper.py
+++ matching_helper.py
@@ -1,10 +1,6 @@
-patterns = ["*,b,*", "a,*,*", "*,*,c", "foo,bar,baz", "w,x,*,*", "*,x,y,z"]
-paths = ["/w/x/y/z/", "a/b/c", "foo/", "foo/bar/", "foo/bar/baz/"]
-
-
 def add_matches(results, cur_matches):
     if not cur_matches:
-        results.append("NO MATCHES")
+        results.append("NO MATCH")
     elif min(cur_matches.keys()) == 0:
         results.append(cur_matches[0][0])
     else:
@@ -20,7 +16,7 @@ def get_best_match(matches, start_index, target):
     
     cur_matches = matches
     
-    left = cur_matches[0].index(target)
+    left = cur_matches[0][start_index:].index(target)
     result = {}
     
     for i in range (0, len(cur_matches)):
@@ -32,39 +28,29 @@ def get_best_match(matches, start_index, target):
                 result[left] = [cur_matches[i]]
         
     if len(result[left]) > 1:
-        return get_best_match(result[left], left + 1, "*")
+        return get_best_match(result[left], start_index + 1, "*")
     else:
         return result[left][0]
-    
-    # x = ((m.index("*"), m) for m in cur_matches)
-    # print(max(((m.index("*"), m) for m in cur_matches))[1])
-        
-    # best_match = max(((m.index("*"), m) for m in cur_matches))[1]
-    # return best_match
 
-# don't need num_patterns or num_paths here
 def get_matching_patterns(patterns, paths):
-    
-    #edge cases first
-    
     results = []
     delimiter = ','
     
-    for i, p in enumerate(paths): # might not need i 
+    for p in paths:
         # remove leading and trailing /
         if p[0] == '/' or p[-1] == '/':
             p = p.strip('/')
             
         pathArr = p.split('/')
         
-        matches = {} # dict O(1) lookup and insertion time
+        matches = {} # using a dict for O(1) lookup and insertion time
         
-        for j, pattern in enumerate(patterns):
+        for pattern in patterns:
             patternArr = pattern.split(',')
             
             if len(patternArr) == len(pathArr):
                 cur_pos = 0
-                cur_match = [] # change name
+                cur_match = []
                 
                 wildcard_count = 0 # will use wildcard count as key in matches dict
                 
@@ -72,14 +58,11 @@ def get_matching_patterns(patterns, paths):
                     if patternArr[cur_pos] == "*":
                         cur_match += patternArr[cur_pos]
                         wildcard_count += 1
-                        # cur_pos += 1
                     elif patternArr[cur_pos] == pathArr[cur_pos]:
                         cur_match.append(patternArr[cur_pos])
-                        # cur_pos += 1
                     else:
-                        # might move where this is invoked
-                        # results.append("NO MATCHES")
                         break
+
                     cur_pos += 1
                 
                 if len(cur_match) == len(pathArr):
@@ -92,6 +75,3 @@ def get_matching_patterns(patterns, paths):
         results = add_matches(results, matches)
         
     return results
-
-    
-# get_matching_patterns(len(patterns), patterns, len(paths), paths)
