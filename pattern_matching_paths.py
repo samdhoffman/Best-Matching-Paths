2e822f2a69b1bbd07ae0ec59b9dd4c0ae1bb5970
Sam Hoffman <samdhoffman@gmail.com>
2020-01-30 15:48:39 -0500

    Check if an input file is piped into terminal command or if stdin has data

--- pattern_matching_paths.py
+++ pattern_matching_paths.py
@@ -1,9 +1,25 @@
 from sys import argv, exit, stdin
+import select
 import matching_helper
 
 
 def process_input():
     input = [line.rstrip() for line in stdin]
+    if len(input) <= 1:
+        print("NO DATA")
+        exit(1)
+
+    # if select.select([stdin],[],[],0.0)[0]:
+    #     print("DATA")
+    # else:
+    #     print("NO DATA")
+
+    # input = []
+    # for line in stdin:
+    #     # print(i)
+    #     print(line)
+    #     # if line
+    
     num_patterns = int(input[0])
     num_paths = int(input[num_patterns + 1])
 
@@ -18,6 +34,12 @@ def process_input():
 
 
 def main():
+    
+    # check if an input file is piped into terminal command
+    if stdin.isatty():
+        print("NO INPUT")
+        exit(1)
+
     patterns, paths = process_input()
 
     matching_patterns = matching_helper.get_matching_patterns(patterns, paths)
@@ -25,7 +47,7 @@ def main():
     for m in matching_patterns:
         print(m)
 
-    exit(1)
+    exit()
 
 if __name__ == "__main__":
     main()
